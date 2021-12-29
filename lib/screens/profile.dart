7d3782d066e1aff6b3b25fb4d5d4cab3e6f9b737
kabita <kabtashrestha0908@gmail.com>
2021-12-29 09:51:04 +0545

    change

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,5 +1,5 @@
 
-// ignore_for_file: avoid_unnecessary_containers, prefer_const_constructors
+// ignore_for_file: avoid_unnecessary_containers, prefer_const_constructors, prefer_const_constructors_in_immutables, use_key_in_widget_constructors
 
 import 'package:flutter/material.dart';
 class Profile extends StatelessWidget {
